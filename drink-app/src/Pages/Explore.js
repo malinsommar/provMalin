b148f559c122641afd9570b500b0b96acf8153d8
malinsommar <malinsommar1997@gmail.com>
2020-07-02 09:34:12 +0200

    Create filter method

--- drink-app/src/Pages/Explore.js
+++ drink-app/src/Pages/Explore.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../Styling/Explore.css";
 import cocktails from "../Data/drinks.json";
 import LinkComponent from "../Components/LinkComponent";
@@ -6,7 +6,6 @@ import CardComponent from "../Components/CardComponent";
 
 const Explore = () => {
   const [shownDrinks, setShownDrinks] = useState([1, 2, 3, 4]);
-  const [input, setInput] = useState();
 
   const printDrinks = () => {
     return shownDrinks.map((item, index) => {
@@ -22,33 +21,34 @@ const Explore = () => {
     });
   };
 
-  const onSearchDrinks = (searchWord) => {
-    cocktails.cocktails.map((item, index) => {
-      if (item.name.includes(searchWord)) {
-        /*setShownDrinks([
-          ...shownDrinks,
-          {
-            id: shownDrinks.length,
-            value: index,
-          },
-        ]);*/
-        //setShownDrinks((shownDrinks) => [...shownDrinks, index]);
-      }
-    });
+  const onSearchDrinks = (e) => {
+    if (e) {
+      e.preventDefault();
+      const searchWord = e.target.elements.input.value.trim().toLowerCase();
+      /*const results = cocktails.cocktails.filter((index) =>
+        index.name.toLowerCase().includes(searchWord)
+      );*/
+      setShownDrinks(filterCocktails(searchWord));
+    }
   };
 
+  const filterCocktails = (searchWord) =>
+    cocktails.cocktails.filter((cocktail) => {
+      return cocktail.name.toLowerCase().includes(searchWord);
+    });
+
   return (
-    <div id="page">
-      <LinkComponent to="/home" title="Home" />
-      <LinkComponent to="/explore" title="Explore" />
-      <h1>Find your favourite drink!</h1>
-      <form>
-        <input className="box" type="text" value={input}></input>
-        <button onClick={onSearchDrinks()} id="searchButton">
-          Search
-        </button>
-      </form>
-      <div id="results">{printDrinks()}</div>
+    <div>
+      <div id="page">
+        <LinkComponent to="/home" title="Home" />
+        <LinkComponent to="/explore" title="Explore" />
+        <h1>Find your favourite drink!</h1>
+        <form onSubmit={onSearchDrinks}>
+          <input name="input" className="box" type="textfield"></input>
+          <button>Search</button>
+        </form>
+        <div id="results">{printDrinks()}</div>
+      </div>
     </div>
   );
 };
